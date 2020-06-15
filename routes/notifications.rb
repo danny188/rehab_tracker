2d23366c4989450d5c7681ac42b9fb4e1682d683
danny188 <dyhh12@gmail.com>
2020-06-15 20:20:25 +1000

    add therapist authentication; display msg if not subcribed

--- routes/notifications.rb
+++ routes/notifications.rb
@@ -1,15 +1,30 @@
 # email notifications
 post "/users/:username/send-notification" do
+  unless verify_user_access(min_authorization: :therapist)
+    redirect "/access_error"
+  end
+
   @event = params[:event]
   @user = User.get(params[:username])
 
+  subscribed = @user.subscriptions[@event.to_sym]
+
   case @event
     when 'exercise_update'
-      @user.send_exercises_updated_email if @user.subscriptions[@event.to_sym]
-      session[:success] = "Exercise Update Notification Email Sent "
+      if subscribed
+        response = @user.send_exercises_updated_email
+
+        if response.status_code == 202
+          session[:success] = "Exercise Update Notification Email Sent"
+        else
+          session[:error] = "Error in sending Exercise Update Notification Email"
+        end
+      else
+        session[:warning] = "User did not subscribe to Exercise Update Notification Email"
+      end
       redirect "/users/#{params[:username]}/exercises"
     when 'exercise_reminder'
-      # @user.send_exercises_reminder_email if @user.subscriptions[@event.to_sym]
+      # @user.send_exercises_reminder_email if subscribed
     when 'new_chat_msg'
   end
 end
